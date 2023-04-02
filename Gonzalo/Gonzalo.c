9b0bcb40c75772a26501a3d23ee0f6839d149dd7
gonzalodr <gonzalodormos26@gmail.com>
2023-04-02 13:01:31 -0600

    solo agregue los comandos a mi archivo

--- Gonzalo/Gonzalo.c
+++ Gonzalo/Gonzalo.c
@@ -2,4 +2,9 @@
     Archivo de Trabajo de Gonzalo. Compilar su archivo con el siguiente comando: 
     gcc -o Gonzalo Gonzalo.c --> Esto para tener un orden al momento de crear los ejecutables
     ./Gonzalo --> Comando para ejecutar el programa
+
+    git status
+    git add nombre_archivo
+    git commit -m "Cualquier mensaje"
+    git push -uf origin main
 */
