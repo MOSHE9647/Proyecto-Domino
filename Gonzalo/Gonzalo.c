9efda17e02d0a72fb6732ee543d9a51d8bbae485
gonzalodr <gonzalodormos26@gmail.com>
2023-04-02 20:03:07 -0600

    terminando metodo arbol y nodo

--- Gonzalo/Gonzalo.c
+++ Gonzalo/Gonzalo.c
@@ -15,37 +15,6 @@
 #include "Metodos_Arbol.h"
 
 int main(){
-    Nodo *n1 = (Nodo *)calloc(sizeof(Nodo),1);
-    n1->dato =(Ficha* )calloc(sizeof(Ficha),1);
-    n1->dato->ficha[0]=0;
-    n1->dato->ficha[1]=0;
-
-    Nodo *n2 = (Nodo *)calloc(sizeof(Nodo),1);
-    n2->dato =(Ficha* )calloc(sizeof(Ficha),1);
-    n2->dato->ficha[0]=1;
-    n2->dato->ficha[1]=6;
-    
-    Nodo *n3= (Nodo *)calloc(sizeof(Nodo),1);
-    n3->dato =(Ficha* )calloc(sizeof(Ficha),1);
-    n3->dato->ficha[0]=2;
-    n3->dato->ficha[1]=5;
-
-    Nodo *n4 = (Nodo *)calloc(sizeof(Nodo),1);
-    n4->dato =(Ficha* )calloc(sizeof(Ficha),1);
-    n4->dato->ficha[0]=3;
-    n4->dato->ficha[1]=4;
-    
-    Nodo *n5 = (Nodo *)calloc(sizeof(Nodo),1);
-    n5->dato =(Ficha* )calloc(sizeof(Ficha),1);
-    n5->dato->ficha[0]=4;
-    n5->dato->ficha[1]=5;
-    Mesa *m = (Mesa *)calloc(sizeof(Mesa),1); 
-
-    AgregarNodoArbol(m,n1,NULL);
-    AgregarNodoArbol(m,n2,n1);
-    AgregarNodoArbol(m,n3,n1);
-    AgregarNodoArbol(m,n4,n1);
-    AgregarNodoArbol(m,n5,n1);
-    Mostrar_Nodos(m);
+   
     return 0;
 }
--- Gonzalo/Metodos_Arbol.h
+++ Gonzalo/Metodos_Arbol.h
@@ -1,59 +1,94 @@
 #ifndef METODOS_ARBOL_H
 #define METODOS_ARBOL_H
 
-void asignacion_recursivo(Nodo *actual, Nodo* nuevo, Nodo *destino){
+/** -----LOS PRIMEROS METODOS SON METODOS INTERNOS OSEA SON METODOS QUE SOLO SE USARAN DENTRO DEL CODIGO DE ESTE ARCHIVO----
+ * Este metodo de guardar nodo recibe una variable el cual indica
+ * si esta guardando en el nodo raiz de la mesa o no
+ * mas adelante incuire otra el cual indica hacia que lado de una ficha par
+ * desea colocar la ficha siguiente "direccion" 1 arriba, 2 siguiente 3 abajo 0 en caso de que sea raiz
+ **/
+Nodo* CreandoNodo(Ficha *domino){
+	Nodo *nuevo = (Ficha *)calloc(sizeof(Ficha),1);
+	nuevo->dato = domino;
+	return nuevo;
+}
+
+void Guardando_Nodo(Nodo *actual, Nodo* nuevo, int direccion){
 	if(actual != NULL){
-		if(actual == destino){
-			if(actual->dato->ficha[0] != actual->dato->ficha[1])
+		if(actual->dato->ficha[0] != actual->dato->ficha[1]){
+			if(actual->siguiente == NULL){
+				nuevo->anterior = actual;
 				actual->siguiente = nuevo;
-         else if(actual->dato->ficha[0] == actual->dato->ficha[1]){
-				if(actual->arriba == NULL)
-					actual->arriba = nuevo;
-				else if(actual->abajo == NULL)
-					actual->abajo = nuevo;
-				else if(actual->siguiente == NULL)
-					actual->siguiente = nuevo;
 			}
-		}else{
-			asignacion_recursivo(actual->arriba, nuevo, destino);
-			asignacion_recursivo(actual->abajo, nuevo, destino);
-			asignacion_recursivo(actual->siguiente, nuevo, destino);
+		}else if(actual->dato->ficha[0] == actual->dato->ficha[1] && actual->cruzado == 1){
+			if(actual->arriba == NULL && direccion == 1){
+				nuevo->anterior = actual;
+				actual->arriba = nuevo;
+			
+			}else if(actual->siguiente == NULL && direccion == 2){
+				nuevo->anterior = actual;
+				actual->siguiente = nuevo;
+
+			}else if(actual->abajo == NULL direccion == 3){
+				nuevo->anterior = actual;
+				actual->abajo = nuevo;
+
+			}else if(actual->anterior == NULL && direccion == 0){
+				nuevo->anterior = actual;
+				actual->anterior = nuevo;
+
+			}
+		}else{	
+			nuevo->anterior = actual;
+			actual->siguiente = nuevo;
 		}
 	}
 }
-	
-void AgregarNodoArbol(Mesa* mesa, Nodo* nuevo, Nodo *destino){
-   Nodo *raiz = mesa->raiz;
- 	if(raiz == NULL){
-      printf("[ %d | %d ] guardado como raiz\n",nuevo->dato->ficha[0],nuevo->dato->ficha[1]);
-		mesa->raiz = nuevo;
-	}else if(raiz == destino){
-		if(raiz->dato->ficha[0] != raiz->dato->ficha[1]){
-			if(raiz->siguiente == NULL)
-            printf("[ %d | %d ] guardado como sig f1 != f2\n",nuevo->dato->ficha[0],nuevo->dato->ficha[1]);
-				mesa->raiz->siguiente = nuevo;
-		}else if(raiz->dato->ficha[0] == raiz->dato->ficha[1]){
-			if(raiz->arriba == NULL){
-           printf("[ %d | %d ] guardado como arriba f1 0= f2\n",nuevo->dato->ficha[0],nuevo->dato->ficha[1]);
-				mesa->raiz->arriba = nuevo;
-         }else if(raiz->abajo == NULL){
-            printf("[ %d | %d ] guardado como abajo f1 != f2\n",nuevo->dato->ficha[0],nuevo->dato->ficha[1]);
-				mesa->raiz->abajo = nuevo;
-         }else if(raiz->siguiente == NULL){
-             printf("[ %d | %d ] guardado como siguiente f1 != f2\n",nuevo->dato->ficha[0],nuevo->dato->ficha[1]);
-				mesa->raiz->siguiente = nuevo;
-         }else if(raiz->anterior ==NULL){
-            printf("[ %d | %d ] guardado como anterior f1 != f2\n",nuevo->dato->ficha[0],nuevo->dato->ficha[1]);
-            mesa->raiz->anterior = nuevo;
-         }
+
+void asignacion_recursivo(Nodo *actual, Nodo* nuevo, Nodo *destino, int direccion){
+	if(actual != NULL){
+		if(actual == destino){
+			Guardando_Nodo(actual, nuevo, direccion);
 		}
 	}else{
-      asignacion_recursivo(mesa->raiz->anterior, nuevo, destino);
-		asignacion_recursivo(mesa->raiz, nuevo, destino);
+		if(actual->dato->ficha[0] == actual->dato->ficha[1]){
+			asignacion_recursivo(actual->arriba, nuevo, destino);
+			asignacion_recursivo(actual->abajo, nuevo, destino);
+		}
+		asignacion_recursivo(actual->siguiente, nuevo, destino);
+	}
+}
+/**
+ * metodo prinsipal que se llamara fuera de este archivo
+ * 
+ **/
+void AgregarNodoArbol(Mesa* mesa, Ficha* domino, Nodo *destino, int direccion){
+	Nodo *nuevo = CreandoNodo(domino);// crea el nodo apartir del la ficha recibida
+ 	if(mesa->raiz == NULL){
+		mesa->raiz = nuevo;
+	}else if(mesa->raiz == destino){
+		Guardando_Nodo(mesa->raiz, nuevo,int direccion);
+	}else{
+      	asignacion_recursivo(mesa->raiz->anterior, nuevo, destino, direccion);
+		asignacion_recursivo(mesa->raiz, nuevo, destino, direccion);
 	}
 }	
+/**
+ * aqui creo la lista con los nodos libres
+ **/
+void ingresar_Lista(Lista *lista, Nodo* Nodo){
 
+}
+void Buscando_fichas_disponibles(){
 
+}
+
+Lista *Fichas_Libres(Lista* lista, Mesa *mesa){
+
+}
+/**
+ * Aquie en  adelante van los metodos de mostreo
+ **/
 void muestra_Recursivo(Nodo *actual){
    if(actual != NULL){
       printf("[ %d | %d ]\n",actual->dato->ficha[0],actual->dato->ficha[1]);
@@ -65,10 +100,11 @@ void muestra_Recursivo(Nodo *actual){
       muestra_Recursivo(actual->abajo);
    }
 }
+
 void Mostrar_Nodos(Mesa *m){
    if(m != NULL){
-      muestra_Recursivo(m->raiz);
-      muestra_Recursivo(m->raiz->anterior);
+		muestra_Recursivo(m->raiz->anterior);
+    	muestra_Recursivo(m->raiz);
    }else{
       printf("Mesa vacia\n");
    }
